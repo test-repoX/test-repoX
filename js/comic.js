976b67d27be7c8dfd4b74b04a37d638e915caa24
test-repoX <121077671+test-repoX@users.noreply.github.com>
2023-02-28 17:09:06 +0400

    eed

--- js/comic.js
+++ js/comic.js
@@ -8,3 +8,10 @@ window.addEventListener("focus", () => {
 });
 
 
+let toggleBtn = document.querySelector("#navbar-toggle");
+let collapse = document.querySelector("#navbar-collapse");
+
+toggleBtn.onclick = () => {
+  collapse.classList.toggle("hidden");
+  collapse.classList.toggle("flex");
+};
